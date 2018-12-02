1d9709fdafb34b99f9cf08c0a0eaedf658d5f08f
Howllow <136344419@qq.com>
2018-12-02 14:14:47 +0800

    init
    update something fundemental to be implemented

--- server.py
+++ server.py
@@ -4,6 +4,52 @@ from asynchat import async_chat
 
 PORT = 3154
 HOST = '127.0.0.1'
+
+class Cmd_Handler:
+    """
+    class to handle different commands
+    """
+    def undefined_cmd(self, cmd):
+
+    def handle_cmd(self, session, cmd):
+
+class EndSession:
+    """
+    do something when a session is ending
+    """
+
+class Room(Cmd_Handler):
+    """
+    general class of ROOM
+    """
+    def __init__(self):
+        self.sessions = []
+
+    def add(self, session):
+        'add a session to the room'
+        self.sessions.append(session)
+
+    def remove(self, session):
+        'remove a session from the room'
+        self.sessions.remove(session)
+
+    def broadcast(self, message):
+        'broadcast the massage to all sessions in the room'
+        for session in self.sessions:
+            session.push(message)
+
+    def handle_logout(self):
+        raise EndSession
+
+
+class LoginHere(Room):
+
+class ChatRoom(Room):
+
+class LogoutHere(Room):
+
+class Session(async_chat):
+
 class Chat_Server(dispatcher):
     def __init__(self):
         dispatcher.__init__(self)
@@ -14,7 +60,7 @@ class Chat_Server(dispatcher):
         self.users = {}
 
 if __name__ == '__main__':
-	s = ChatServer(PORT)
+	s = Chat_Server(PORT)
 	try:
 		asyncore.loop()
 	except KeyboardInterrupt:
