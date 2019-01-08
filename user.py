d2b3f85f3524a0a0e699354a5b4c0e7b704168a1
Howllow <136344419@qq.com>
2019-01-08 13:58:18 +0800

    user
    usermanager

--- user.py
+++ user.py
@@ -3,7 +3,17 @@ import flask_login as fl
 class User(fl.UserMixin):
     def __init__(self):
         self.username = None
-
+        self.id = None
     def __repr__(self):
         return '<User %r>' % self.username
 
+class UserManager:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get(self, id):
+        user = User()
+        user.username = id
+        user.id = id
+        return user
